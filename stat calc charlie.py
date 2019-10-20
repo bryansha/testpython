81e9a8f26100d46c1a03d753502c856a26ce1279
Bryan Ha <30011082+bryansha@users.noreply.github.com>
2019-10-19 22:24:49 -0300

    Update stat calc charlie.py

--- stat calc charlie.py
+++ stat calc charlie.py
@@ -1,15 +1,14 @@
 count = 0
-list1 = []
+
 go = True
 while go:
-    list1 = input("put your numbers here: ")
+    list1 = list(input("put your numbers here: "))
     length = len(list1)
-    print(length)
+    print("You entered", length, "numbers")
     count += 1
     print(list1)
     ques = input("Anything else? ")
 
 
-    if ques == "":
-        go = False
+
 
